9d566244c35cb28b209b46be207423a87fa800e7
thanh1v <tp803709@gmail.com>
2025-10-16 21:43:20 +0700

    Update main.py

--- secure-dir/main.py
+++ secure-dir/main.py
@@ -5,6 +5,17 @@ import sys
 import json
 import os
 
+# Đọc file .encrypt.py
+import importlib.util
+current_dir = os.path.dirname(os.path.abspath(__file__))
+module_path = os.path.join(current_dir, ".encrypt.py")
+
+spec = importlib.util.spec_from_file_location("aes", module_path)
+aes = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(aes)
+# Gọi hàm trong file
+# aes.ten_ham_can_dung()
+
 
 MAX_ATTEMPTS = 3
 
@@ -40,8 +51,10 @@ def authenticate():
 
         if username_input in accounts:
             account_data = accounts[username_input]
+            enc_password = account_data.get("password")
+            plt_password = aes.aes_decrypt(aes.key, enc_password)
 
-            if password_input == account_data.get("password"):
+            if password_input == plt_password:
                 role = account_data.get("role")                     # chưa sử dụng!!
                 print("=== Đăng nhập thành công ===")
                 print(f"Chào mừng {username_input}")
@@ -55,6 +68,8 @@ def authenticate():
                 print("Bạn đã nhập sai 3 lần. Truy cập bị từ chối.")
 
     return False
+
+
 
 if __name__ == "__main__":
     if authenticate():
